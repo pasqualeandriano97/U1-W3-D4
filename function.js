aab95c5b110d0c3b20b7e2d61aeeb505dfab320d
pasqualeandriano97 <andrianopasquale97@gmail.com>
2023-12-08 17:52:42 +0100

    prime modifiche

--- function.js
+++ function.js
@@ -1,8 +1,10 @@
 console.log("prova");
-
+const numeriPossibili = [];
 const createAllNumber = function () {
   const numberTabel = document.getElementById("allNumbers");
   for (let i = 0; i < 90; i++) {
+    numeriPossibili.push(i + 1);
+
     const numberDiv = document.createElement("div");
     const number = document.createElement("h3");
     number.classList.add("number");
@@ -14,9 +16,7 @@ const createAllNumber = function () {
 };
 
 createAllNumber();
-const numeriEstratti = [];
-const numeriusciti = [];
-const numeriDoppi = [];
+
 const randomF = function () {
   const randomDiv = document.getElementById("random");
   const button = document.createElement("button");
@@ -24,20 +24,11 @@ const randomF = function () {
   randomDiv.appendChild(button);
   button.addEventListener("click", function () {
     const randomNumber = Math.floor(Math.random() * 90);
-    numeriEstratti.push(randomNumber);
-    // console.log(randomNumber);
-    for (let i = 0; i < numeriEstratti.length; i++) {
-      if (numeriEstratti[i] !== numeriusciti.length) {
-        numeriusciti.push(numeriEstratti[i]);
-        const number = document.getElementsByTagName("h3")[randomNumber];
-        number.classList.add("marker");
-      } else {
-        numeriDoppi.push(numeriEstratti[0]);
-      }
-    }
-
-    console.log("u", numeriusciti);
-    console.log("d", numeriDoppi);
+    const marker =
+      document.getElementsByClassName("number")[parseInt(randomNumber)];
+    marker.classList.add("marker");
+    numeriPossibili.splice(parseInt(randomNumber), 1);
   });
 };
 randomF();
+console.log("numeri possibili", numeriPossibili);
